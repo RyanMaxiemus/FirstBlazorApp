4d7f63a1257e8df30c6f2973e16eb35b3dc2cd06
Ryan Maxie <MichaelRMaxie@Gmail.com>
2024-12-24 21:42:18 -0600

    Implimented variable  into counter. Created counter onto home page with an incrementation of 10. Created a weather forecast on the home page.

--- FirstBlazorApp/obj/Debug/net9.0/FirstBlazorApp.AssemblyInfo.cs
+++ FirstBlazorApp/obj/Debug/net9.0/FirstBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acfba6bf12ddd72805913c991da6dc06995b32b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
